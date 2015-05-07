b76edfbb284db5da60c11433d2d8bf38144414fa
Aaron Sawyer <me@aarondevon.com>
2015-05-07 17:51:16 -0500

    adds script ap.js to html head

---
+++ server/public/assets/scripts/app.js
@@ -0,0 +1,11 @@
+
+
+
+
+
+
+
+
+$( document ).ready(function() {
+    $('.luce-info').append('<ul class="slide"><li>Responsible for delivery of food, accurate order taking, and friendly customer service.</li></ul>');
+});
