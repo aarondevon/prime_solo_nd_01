2de6beb850c0650d1ae1ac0a771e882ce6cb78aa
Aaron Sawyer <me@aarondevon.com>
2015-05-07 14:36:24 -0500

    Adds serve/app.js code

--- server/app.js
+++ server/app.js
@@ -6,5 +6,10 @@ var path = require('path');
 app.set('port', (process.env.PORT || 3000));
 
 app.get('/*', function(request, response){
-    var file = request.params[0] || 'views/index.html'
+    var file = request.params[0] || 'views/index.html';
+    response.sendFile(path.join(__dirname, './public', file));
+});
+
+app.listen(app.get('port'), function(){
+    console.log("App is running on port: ", app.get('port'));
 });
