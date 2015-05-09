4a1d5389a13efcda49c70df3a0038bb9b92b0979
Aaron Sawyer <me@aarondevon.com>
2015-05-09 15:13:35 -0500

    Adds teplate html file and ajax call

--- server/public/assets/scripts/app.js
+++ server/public/assets/scripts/app.js
@@ -1,12 +1,13 @@
-
-
-
-
-
-
-
-
+var contact;
 $( document ).ready(function() {
+    $.ajax({
+        method : "GET",
+        url: "/views/contact.html",
+        success: function(data) {
+            $(".contact").append(data);
+        }
+    });
+
     $('.luce-info').append('<ul class="col-md-12 right"><li>Responsible for delivery of food, accurate order taking, and friendly customer service.</li></ul>');
     $( "li, p" ).hover(
         function() {
